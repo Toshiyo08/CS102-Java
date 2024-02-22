a1ae88c254340f39733086218937352d62598e2f
Toshiyo08 <toshiyo0806@gmail.com>
2024-02-22 13:54:29 +0800

    Updated Card
    Card class updated

--- Card.java
+++ Card.java
@@ -1,19 +1,48 @@
+// package CS102cardgame.src;
+
 public class Card {
-    private String Suit;
-    private String Value;
+    private int number;
+    private String suit;
+
+    public Card(int number, String suit) {
+        this.suit = suit;
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return this.number;
+    }
 
     public String getSuit() {
-        return Suit;
+        return this.suit;
     }
 
-    public String getValue() {
-        return Value;
+    public void setNumber(int number) {
+        this.number = number;
     }
 
-    public Card(String suit, String value) {
-        this.Suit = suit;
-        this.Value = value;
+    public void setSuit(String suit) {
+        this.suit = suit;
     }
 
-    
+    public String toString() {
+        String name = "";
+
+        if (this.number > 10) {
+            if (this.number == 11) {
+                name += "Jack";
+            } else if (this.number == 12) {
+                name += "Queen";
+            } else if (this.number == 13) {
+                name += "King";
+            } else if (this.number == 14) {
+                name += "Ace";
+            }
+        } else {
+            name += this.number;
+        }
+        name += " of " + this.suit;
+
+        return name;
+    }
 }
